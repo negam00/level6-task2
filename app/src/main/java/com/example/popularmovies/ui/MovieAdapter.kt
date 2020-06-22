70979d9c4f01c726f07957d2ab83815f64acb4c3
negam00 <negam00@github.com>
2020-06-22 21:08:39 +0200

    added movie adapter

---
+++ app/src/main/java/com/example/popularmovies/ui/MovieAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.popularmovies.ui
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.popularmovies.R
+import com.example.popularmovies.model.Movie
+import kotlinx.android.synthetic.main.item_movie.view.*
+
+class MovieAdapter(private val movies: List<Movie>, private val onClick: (Movie) -> Unit) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+    private lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: MovieAdapter.ViewHolder, position: Int) {
+        holder.bind(movies[position], position + 1)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener { onClick(movies[adapterPosition]) }
+        }
+        fun bind(movie: Movie, position: Int) {
+            itemView.tvMovieNumber.text = position.toString()
+            Glide.with(context).load(movie.posterImage).into(itemView.ivPosterItem)
+        }
+    }
+}
