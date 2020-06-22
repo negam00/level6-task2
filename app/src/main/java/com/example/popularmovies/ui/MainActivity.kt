bb009d6c843477a8bc447075107248e10307a7bd
negam00 <negam00@github.com>
2020-06-22 22:17:47 +0200

    initialized views

--- app/src/main/java/com/example/popularmovies/ui/MainActivity.kt
+++ app/src/main/java/com/example/popularmovies/ui/MainActivity.kt
@@ -1,36 +1,43 @@
 package com.example.popularmovies.ui
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.popularmovies.R
-
+import com.example.popularmovies.model.Movie
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
+const val EXTRA_MOVIE = "EXTRA_MOVIE"
 class MainActivity : AppCompatActivity() {
+    private val movies = arrayListOf<Movie>()
+    private val movieAdapter = MovieAdapter(movies) { movie -> onMovieItemClick(movie) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        initViews()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun initViews() {
+        val gridLayoutManager = GridLayoutManager(this, 1, RecyclerView.VERTICAL, false)
+        rvMovies.layoutManager = gridLayoutManager
+        rvMovies.adapter = movieAdapter
+
+        btnMovieYear.setOnClickListener { onSumbitClick() }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun onSumbitClick() {
+
+    }
+
+    private fun onMovieItemClick(movie: Movie) {
+        val intent = Intent(this, DetailsActivity::class.java)
+        intent.putExtra(EXTRA_MOVIE, movie)
+        startActivityForResult(intent, 100)
     }
 }
